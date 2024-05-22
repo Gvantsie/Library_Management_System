a1442c8260398615fd519b9cd80350d944616c78
Gvantsie <g_euashvili@cu.edu.ge>
2024-05-23 03:20:40 +0400

    createsuperuser error solved

--- users/models.py
+++ users/models.py
@@ -4,8 +4,8 @@ from django.db import models
 
 # Create your models here.
 class CustomUser(AbstractUser):
-    personal_number = models.CharField(max_length=20, unique=True)
-    birth_date = models.DateField()
+    personal_number = models.CharField(max_length=11, unique=True)
+    birth_date = models.DateField(null=True)
 
     def __str__(self):
         return self.email
