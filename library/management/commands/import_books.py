727e4b92dfb78e4fb824bdd97286cafc8c4b7ce0
Gvantsie <g_euashvili@cu.edu.ge>
2024-05-24 21:11:10 +0400

    books rendering-DONE

--- library/management/commands/import_books.py
+++ library/management/commands/import_books.py
@@ -1,47 +1,37 @@
-import concurrent.futures
-import random
-from django.core.management.base import BaseCommand
-from django.db import transaction, IntegrityError
-from utils.factories import create_authors, BookFactory, GENRE_CHOICES
-from library.models.genre import Genre
+import threading
+
+from django.core.management import BaseCommand
+
+from utils.factories import import_books
 
 
 class Command(BaseCommand):
-    help = 'Import books into the database using concurrent features'
+    help = 'Imports books into the database(Command <num_books> <num_threads>).'
 
     def add_arguments(self, parser):
         parser.add_argument('num_books', type=int, help='Number of books to import')
+        parser.add_argument('num_threads', type=int, help='Number of threads to use')
 
     def handle(self, *args, **options):
         num_books = options['num_books']
-        num_authors = num_books // 5  # Example ratio of authors to books
+        num_threads = options['num_threads']
 
-        self.stdout.write(self.style.NOTICE('Creating authors...'))
-        authors = create_authors(num_authors)
+        # Calculate the number of books per thread
+        books_per_thread = num_books // num_threads
 
-        def create_book(_):
-            author = random.choice(authors)
-            book = BookFactory(author=author)
-            genres = random.choices(GENRE_CHOICES, k=random.randint(1, 3))
-            book.save()
+        threads = []
+        for i in range(num_threads):
+            if i == num_threads - 1:
+                # The last thread will handle the remaining books
+                num_books_for_thread = num_books - (books_per_thread * i)
+            else:
+                num_books_for_thread = books_per_thread
 
-            for genre_name in genres:
-                self.add_genre_to_book(book, genre_name)
+            thread = threading.Thread(target=import_books, args=(num_books_for_thread,))
+            threads.append(thread)
+            thread.start()
 
-            return book
-
-        self.stdout.write(self.style.NOTICE('Creating books...'))
-        with concurrent.futures.ThreadPoolExecutor() as executor:
-            futures = [executor.submit(create_book, i) for i in range(num_books)]
-            for future in concurrent.futures.as_completed(futures):
-                try:
-                    future.result()
-                except Exception as e:
-                    self.stdout.write(self.style.ERROR(f'Error creating book: {e}'))
+        for thread in threads:
+            thread.join()
 
         self.stdout.write(self.style.SUCCESS(f'Successfully imported {num_books} books.'))
-
-    def add_genre_to_book(self, book, genre_name):
-        with transaction.atomic():
-            genre, created = Genre.objects.select_for_update().get_or_create(genre_name=genre_name)
-            book.genre.add(genre)  # Use 'genre' instead of 'genres'
--- utils/factories.py
+++ utils/factories.py
@@ -1,12 +1,18 @@
 import factory
 from faker import Faker
-from django.utils import timezone
+
 from library.models.author import Author
 from library.models.book import Book
 from library.models.genre import Genre
 
-
 fake = Faker()
+
+
+class GenreFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Genre
+
+    genre_name = factory.Faker('word')
 
 
 class AuthorFactory(factory.django.DjangoModelFactory):
@@ -15,37 +21,23 @@ class AuthorFactory(factory.django.DjangoModelFactory):
 
     first_name = factory.Faker('first_name')
     last_name = factory.Faker('last_name')
-    date_of_birth = factory.Faker('date_of_birth', tzinfo=timezone.get_current_timezone())
-    date_of_death = factory.LazyAttribute(lambda obj: fake.date_between(start_date=obj.date_of_birth, end_date='+90y')
-                                            if fake.boolean(chance_of_getting_true=30) else None)
-
-
-def create_authors(num_authors):
-    return AuthorFactory.create_batch(num_authors)
+    date_of_birth = factory.Faker('date_of_birth', minimum_age=18)
+    date_of_death = factory.Iterator([fake.date_this_decade(before_today=True) for _ in range(50)])
 
 
 class BookFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Book
 
-    title = factory.Faker('catch_phrase')
-    description = factory.Faker('text')
-    stock = factory.Faker('random_int', min=1, max=100)
-    publication_date = factory.Faker('date')
-    cover_type = factory.Faker('random_element', elements=['hard', 'soft'])
-    status = 'available'
     author = factory.SubFactory(AuthorFactory)
+    genre = factory.RelatedFactoryList(GenreFactory, size=lambda: fake.random_int(min=1, max=3))
+    title = factory.Faker('sentence', nb_words=6)
+    description = factory.Faker('text', max_nb_chars=200)
+    stock = factory.Faker('random_int', min=1, max=100)
+    publication_date = factory.Faker('date_this_decade', before_today=True)
+    cover_type = factory.Iterator(['hard', 'soft'])
+    status = 'available'
 
 
-GENRE_CHOICES = [
-    'Fantasy', 'Science Fiction', 'Mystery', 'Thriller', 'Romance',
-    'Historical Fiction', 'Horror', 'Biography', 'Self-Help', 'Travel',
-    'Cooking', 'Art', 'Poetry', 'Drama'
-]
-
-
-class GenreFactory(factory.django.DjangoModelFactory):
-    class Meta:
-        model = Genre
-
-    genre_name = factory.Faker('random_element', elements=GENRE_CHOICES)
+def import_books(num_books):
+    books = BookFactory.create_batch(num_books)
